754b3c57bfdbda2989e59b0fca2dd9cf1c7150ae
yunjulanju <127844917+yunjulanju@users.noreply.github.com>
2026-07-07 11:49:26 +0900

    fix : 주석 정리

--- Source/Valheim/Item/ItemBase.h
+++ Source/Valheim/Item/ItemBase.h
@@ -17,15 +17,12 @@ class VALHEIM_API AItemBase : public AActor, public IInteractable
 
     //------------------------------Function ////
 public:
-    // Sets default values for this actor's properties
     AItemBase();
 
-    // Called every frame
     virtual void Tick(float DeltaTime) override;
 
     void InitializeItem(const int32 InQuantity);
 
-    // 필드에서 드롭될 때 호출: 어떤 아이템인지는 이제 UObject가 아니라 ItemID로 전달받음
     void InitiallizeDrop(FName InItemID, const int32 InQuantity);
 
     FORCEINLINE FName GetItemID() const { return ItemID; }
@@ -37,7 +34,6 @@ public:
     virtual void Interact(APawn* Interactor) override;
 
 protected:
-    // Called when the game starts or when spawned
     virtual void BeginPlay() override;
 
     virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
@@ -47,7 +43,6 @@ protected:
     UFUNCTION()
     void OnRep_CurrentMeshAsset();
 
-    // ItemID로 인벤토리에 넣기를 시도하고, 성공하면 true (호출부에서 Destroy 여부 결정)
     bool TryAddToInventory(AArcher* Taker);
 
     //------------------------------Property ////
--- Source/Valheim/Item/ItemSubsystem.h
+++ Source/Valheim/Item/ItemSubsystem.h
@@ -16,21 +16,17 @@ class VALHEIM_API UItemSubsystem : public UGameInstanceSubsystem
 public:
 	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
 
-	// ItemID로 정적 아이템 데이터 조회
 	UFUNCTION(BlueprintCallable, Category = "Item")
 	bool GetItemData(FName ItemID, UItemPrimaryDataAsset*& OutData) const;
 
-	// 존재 여부만 확인
 	UFUNCTION(BlueprintCallable, Category = "Item")
 	bool DoesItemExist(FName ItemID) const;
 
 protected:
-	// 모든 아이템 애셋 목록을 담은 레지스트리 (GameInstance에서 받아옴)
 	UPROPERTY()
 	TObjectPtr<UItemRegistry> ItemRegistry;
 
 private:
-	// 빠른 조회용 캐시: ItemID → 실제 애셋
 	TMap<FName, TObjectPtr<UItemPrimaryDataAsset>> ItemDataCache;
 
 	void BuildItemCache();

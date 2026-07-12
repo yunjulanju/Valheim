43266154fa6eaa895446e5242191c1856627a76b
yunjulanju <yunju6528@duksung.ac.kr>
2026-07-12 17:07:35 +0900

    feat : 클라이언트 서버 연결 코드 추가

--- Source/Valheim/Character/ArcherPC.cpp
+++ Source/Valheim/Character/ArcherPC.cpp
@@ -2,3 +2,76 @@
 
 
 #include "Character/ArcherPC.h"
+#include "TCP/TCPClientSubSystem.h"
+
+AArcherPC::AArcherPC()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void AArcherPC::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (!IsLocalController())
+	{
+		return; // 서버가 들고 있는 원격 클라이언트용 PC 인스턴스는 여기서 끝
+	}
+
+	ENetMode Mode = GetNetMode();
+
+	if (Mode == NM_Client)
+	{
+		// 순수 클라이언트 → TCP 클라이언트로 접속
+		UTCPClientSubsystem* TCP = GetTCP();
+		TCP->OnTCPConnected.AddDynamic(this, &AArcherPC::HandleConnected);
+		TCP->OnTCPDisconnected.AddDynamic(this, &AArcherPC::HandleDisconnect);
+		TCP->Connect(TEXT("127.0.0.1"), 35000);
+	}
+	else if (Mode == NM_ListenServer)
+	{
+		// 호스트 자신 → 채팅 서버 오픈 (TCPServerSubsystem 만들어지면 여기서 StartListen)
+		// UTCPServerSubsystem* Server = GetGameInstance()->GetSubsystem<UTCPServerSubsystem>();
+		// Server->StartListen(35000);
+	}
+}
+
+void AArcherPC::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	UTCPClientSubsystem* TCP = GetTCP();
+
+	TCP->OnTCPConnected.RemoveDynamic(this, &AArcherPC::HandleConnected);
+	TCP->OnTCPDisconnected.RemoveDynamic(this, &AArcherPC::HandleDisconnect);
+
+	Super::EndPlay(EndPlayReason);
+}
+
+void AArcherPC::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+UTCPClientSubsystem* AArcherPC::GetTCP()
+{
+	UGameInstance* GI = GetGameInstance();
+	if (GI)
+	{
+		return GI->GetSubsystem<UTCPClientSubsystem>();
+	}
+
+	return nullptr;
+}
+
+void AArcherPC::HandleConnected()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Connected Server"));
+
+	UTCPClientSubsystem* TCP = GetTCP();
+
+	//TCP->SendLogin(TEXT("junios"), TEXT("1234"));
+}
+
+void AArcherPC::HandleDisconnect()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Disonnected Server"));
+}
--- Source/Valheim/Character/ArcherPC.h
+++ Source/Valheim/Character/ArcherPC.h
@@ -9,10 +9,30 @@
 /**
  * 
  */
+class UTCPClientSubsystem;
 UCLASS()
 class VALHEIM_API AArcherPC : public APlayerController
 {
 	GENERATED_BODY()
 
+public:
+	AArcherPC();
+
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintCallable)
+	UTCPClientSubsystem* GetTCP();
+
+	UFUNCTION()
+	void HandleConnected();
+
+	UFUNCTION()
+	void HandleDisconnect();
 
 };
